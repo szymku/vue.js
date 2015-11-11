356c6da4b07fffe76141327a486c11d79cb01147
szymkul <szymkul@gmail.com>
2015-11-11 18:02:45 +0100

    easy filtering and ordering

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 new Vue({
 
-	el: '#post',
+	el: '#demo',
+
 
 	data: {
-		isVisible: true,
-		liked: false,
-		likesCount: 10
+		sortKey: '',
+
+		reverse: false,
+
+		search: '',
+
+		columns: ['name', 'age'],
+
+		people:[
+			{name: 'John', age: 23},
+			{name: 'Tom', age: 43},
+			{name: 'Jo', age: 25},
+			{name: 'Brian', age: 63},
+			{name: 'Igor', age: 13},
+			{name: 'Eve', age: 21},
+			{name: 'Zet', age: 52},
+			{name: 'Anton', age: 33}
+		]
 	},
 
-	methods:{
+	methods: {
+		sortBy: function(sortKey){
+			this.reverse = (this.sortKey == sortKey) ? ! this.reverse : false;
+			this.sortKey = sortKey;
 
-		toggleLike: function() {
-
-			this.liked = ! this.liked;
-			this.liked ? this.likesCount++ : this.likesCount--;
 		}
-
 	}
-
-
-
-
 });
